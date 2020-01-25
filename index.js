284ec7ea21f95f5706fe81e1d1c4e554e936174f
Ana Karki <karki9321@gmail.com>
2020-01-25 09:42:47 -0700

    initial comit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const exphbs  = require('express-handlebars');
+
+// Set Handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+//Set Handlebars route
+
+app.get('/', function (req, res) {
+    res.render('home', {
+    		Ana : "This is all about Ana.."
+    });
+});
+const PORT = process.env.PORT || 3000;
+//Set a static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen((PORT), () => console.log ('Listening on the port '+ PORT));
